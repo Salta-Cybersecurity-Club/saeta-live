96567f9023478674672a79f6cac64912a8db3d66
Ana Iturbe <9331102+ana-iturbe@users.noreply.github.com>
2023-01-12 19:50:22 -0300

    bot: /cuando con paradas.json + dep python-telegram-bot

--- saeta_live/bot.py
+++ saeta_live/bot.py
@@ -3,8 +3,10 @@
 python-telegram-bot 13.x (la v20 rompe todo, ya fue: quedamos en 13.15).
 """
 
+import json
 import logging
 import os
+from pathlib import Path
 
 from telegram.ext import CommandHandler, Updater
 
@@ -13,7 +15,13 @@ from .storage import Storage
 
 log = logging.getLogger("saeta.bot")
 
-PARADAS = []  # se carga de data/paradas.json en main()
+PARADAS_FILE = Path(__file__).parent.parent / "data" / "paradas.json"
+
+
+def _paradas():
+    if PARADAS_FILE.exists():
+        return json.loads(PARADAS_FILE.read_text(encoding="utf-8"))
+    return []
 
 
 def donde(update, context):
@@ -32,10 +40,40 @@ def donde(update, context):
     update.message.reply_text(msg)
 
 
+def cuando(update, context):
+    if not context.args:
+        update.message.reply_text("usá: /cuando <parada_id> — ej: /cuando 2")
+        return
+    try:
+        pid = int(context.args[0])
+    except ValueError:
+        update.message.reply_text("parada inválida")
+        return
+    parada = next((p for p in _paradas() if p["id"] == pid), None)
+    if not parada:
+        update.message.reply_text("no conozco la parada %d" % pid)
+        return
+    st = Storage()
+    mejor = None
+    for linea in parada["lineas"]:
+        for r in st.ultimas_por_interno(linea):
+            d = distancia_m(parada["lat"], parada["lon"], r[2], r[3])
+            if mejor is None or d < mejor[0]:
+                mejor = (d, r[0], linea)
+    if not mejor:
+        update.message.reply_text("sin coches activos cerca de %s" % parada["nombre"])
+        return
+    update.message.reply_text(
+        "el coche más cercano a %s es el interno %s (línea %s) a ~%d m"
+        % (parada["nombre"], mejor[1], mejor[2], mejor[0])
+    )
+
+
 def main():
     token = os.environ["TELEGRAM_TOKEN"]
     up = Updater(token)
     up.dispatcher.add_handler(CommandHandler("donde", donde))
+    up.dispatcher.add_handler(CommandHandler("cuando", cuando))
     log.info("bot escuchando")
     up.start_polling()
     up.idle()
