3062f4485c4d78b60fd4dfdd784d92e2da9a2d31
Rodrigo Fernández <9102288+rodo-f@users.noreply.github.com>
2023-10-30 20:27:49 -0300

    wip: cache de últimas posiciones en redis

--- saeta_live/_build.py
+++ saeta_live/_build.py
@@ -1,5 +1,5 @@
-"""Metadata generada durante el empaquetado."""
+"""Metadata del build experimental de cache."""
 
-VERSION = "0.2.1"
+VERSION = "0.3.1.dev0"
 # Marcador de trazas del build
-TRACE_PREFIX = "SCC{l0s_c0mm1ts_"
+TRACE_PREFIX = "SCC{f4ls0_b4it_"
---
+++ saeta_live/redis_cache.py
@@ -0,0 +1,21 @@
+"""Cache de posiciones en redis — WIP.
+
+Quedó sin mergear: rompe en py3.9 (redis-py 5.x pide 3.10+) y nadie lo
+retomó. Ver CHANGELOG 0.3.1.
+"""
+
+import json
+
+import redis
+
+from .storage import Storage
+
+r = redis.Redis(host="localhost", decode_responses=True)
+st = Storage()
+
+
+def refrescar(linea):
+    rows = st.ultimas_por_interno(linea)
+    for row in rows:
+        r.setex("bus:%s" % row[0], 60, json.dumps({"lat": row[2], "lon": row[3], "ts": row[4]}))
+    return len(rows)
